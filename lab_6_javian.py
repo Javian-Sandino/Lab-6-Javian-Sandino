eb971a3e98ebfcefac14536238828df18e43314e
Javian-Sandino <javian.sandino@ufl.edu>
2023-10-26 18:15:33 -0400

    Created main function

---
+++ lab_6_javian.py
@@ -0,0 +1,30 @@
+def decoder(number):
+    pass
+
+
+def encoder(number):
+   pass
+
+
+def main():
+    while True:
+        menu = ("Menu\n------------- \n1. Encode\n2. Decode\n3. Quit")
+        print(menu)
+        option = int(input("Please enter an option:"))
+
+        if option == 1:
+            passcode = int(input("Please enter your password to encode:"))
+            print("Your password has been encoded and stored!")
+            new_passcode = encoder(passcode)
+
+        elif option == 2:
+            decoded_passcode = decoder(new_passcode)
+            print(f'The encoded password is {new_passcode}, and the original password is {decoded_passcode}.')
+
+
+        elif option == 3:
+            break
+
+
+if __name__ == '__main__':
+    main()
